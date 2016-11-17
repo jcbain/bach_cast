75b4dbd210067ed9ef37d9e2c2bc3296481897b6
James Bain <jamescbain@gmail.com>
2016-11-17 17:01:39 -0600

    turn data_grabber into a class

--- data_grabber.py
+++ data_grabber.py
@@ -4,28 +4,26 @@ import pandas as pd
 import numpy as np
 import html5lib
 
+class RawProcessor:
 
 
 
-def rawWriter(url,i, show = "bachelorette"):
-    name = requests.get(url)
+    def rawWriter(url,i, show = "bachelorette"):
+        name = requests.get(url)
 
-    with open("/Users/jamesbain/Desktop/bachelor/data/raw/{}{}.html".format(show,i), "wb") as f:
-        f.write(name.content)
+        with open("/Users/jamesbain/Desktop/bachelor/data/raw/{}{}.html".format(show,i), "wb") as f:
+            f.write(name.content)
 
-# example
-# -------
-# rawWriter('https://en.wikipedia.org/wiki/The_Bachelor_(season_1)',1,show = "bachelor")
+    # example
+    # -------
+    # rawWriter('https://en.wikipedia.org/wiki/The_Bachelor_(season_1)',1,show = "bachelor")
 
-def rawReader(file):
+    def rawReader(file):
 
-    with open(file, "rb") as f:
-        soup = BeautifulSoup(f, "lxml")
-    return soup
+        with open(file, "rb") as f:
+            soup = BeautifulSoup(f, "lxml")
+        return soup
 
-# example
-# -------
-#soup = rawReader("/Users/jamesbain/Desktop/bachelor/data/raw/bachelor11.html")
-
-if __name__ == '__main__':
-    main()
+    # example
+    # -------
+    #soup = rawReader("/Users/jamesbain/Desktop/bachelor/data/raw/bachelor11.html")
