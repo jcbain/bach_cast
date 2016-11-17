199db847d7ab6268ce5712d339e207eab76d6be5
James Bain <jamescbain@gmail.com>
2016-11-17 16:47:18 -0600

    add verbiage

--- data_grabber.py
+++ data_grabber.py
@@ -1,5 +1,13 @@
-def rawWriter(url,i, show = "bachelorette"):
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import numpy as np
+import html5lib
 
+
+
+
+def rawWriter(url,i, show = "bachelorette"):
     name = requests.get(url)
 
     with open("/Users/jamesbain/Desktop/bachelor/data/raw/{}{}.html".format(show,i), "wb") as f:
@@ -9,7 +17,15 @@ def rawWriter(url,i, show = "bachelorette"):
 # -------
 # rawWriter('https://en.wikipedia.org/wiki/The_Bachelor_(season_1)',1,show = "bachelor")
 
+def rawReader(file):
 
+    with open(file, "rb") as f:
+        soup = BeautifulSoup(f, "lxml")
+    return soup
+
+# example
+# -------
+#soup = rawReader("/Users/jamesbain/Desktop/bachelor/data/raw/bachelor11.html")
 
 if __name__ == '__main__':
     main()
